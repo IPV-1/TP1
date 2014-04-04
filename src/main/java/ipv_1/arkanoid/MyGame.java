5e4f4e20d3ab80d4afdbecfb8d0ca2afe2ff72c9
Mariano <marianolinares92@gmail.com>
2014-04-04 17:33:07 -0300

    primera implementacion de rebote en X e Y

--- src/main/java/ipv_1/arkanoid/MyGame.java
+++ src/main/java/ipv_1/arkanoid/MyGame.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 
 import com.uqbar.vainilla.DeltaState;
+import com.uqbar.vainilla.DesktopGameLauncher;
 import com.uqbar.vainilla.Game;
 import com.uqbar.vainilla.GameComponent;
 import com.uqbar.vainilla.GameScene;
@@ -12,6 +13,10 @@ import com.uqbar.vainilla.appearances.Circle;
 public class MyGame extends Game {
 
 	private Dimension dimension;
+	
+	public static void main(String[] args) throws Exception {
+		new DesktopGameLauncher(new MyGame()).launch();
+	}
 
 	@Override
 	protected void initializeResources() {
@@ -23,18 +28,29 @@ public class MyGame extends Game {
 	protected void setUpScenes() {
 		GameScene scene = new GameScene();
 		final int diameter = 50;
-		scene.addComponent(new GameComponent<GameScene>(new Circle(Color.blue, diameter), 0, 290){
-			int direction = 1;
-			double velocity = 100; 
+		scene.addComponent(new GameComponent<GameScene>(new Circle(Color.blue, diameter), 0, 290) {
+			int directionX = 1;
+			int directionY = 1;
+			double velocity = 100;
+			
 			@Override
 			public void update(DeltaState deltaState) {
-				this.setX(this.getX() + direction*velocity*deltaState.getDelta());
+				this.setX(this.getX() + directionX*velocity*deltaState.getDelta());
 				if(this.getX() <= 0 ) {
 					this.setX(0);
-					direction = -direction;
+					directionX = -directionX;
 				} else if(this.getX() + diameter >=dimension.width) {
 					this.setX(dimension.width - diameter);
-					direction = -direction;					
+					directionX = -directionX;					
+				}
+				
+				this.setY(this.getY() + directionY*velocity*deltaState.getDelta());
+				if(this.getY() <= 0 ) {
+					this.setY(0);
+					directionY = -directionY;
+				} else if(this.getY() + diameter >=dimension.height) {
+					this.setX(dimension.height - diameter);
+					directionY = -directionY;					
 				}
 			}
 		});
