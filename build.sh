80efddb112ba3cb9b5204209ebd29576debb8300
Mariano <marianolinares92@gmail.com>
2014-04-12 14:01:29 -0300

    Build vainilla

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+cd $CI_HOME/vainilla
+mvn clean install
+
 cd $CI_HOME/arkanoid
 mvn install:install-file -DgroupId=com.uqbar -DartifactId=vainilla -Dversion=0.0.4 -Dpackaging=jar -Dfile=../vainilla/classes/artifacts/vainilla_jar/vainilla.jar
 mvn install
