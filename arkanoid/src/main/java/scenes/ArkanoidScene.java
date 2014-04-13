cffa5933ae12b61b51bd7a54fae95412399f8b02
lalo <leandro.gz73@gmail.com>
2014-04-12 23:58:25 -0300

    Add BasicArkanoidScene class

--- arkanoid/src/main/java/scenes/ArkanoidScene.java
+++ arkanoid/src/main/java/scenes/ArkanoidScene.java
@@ -1,78 +1,23 @@
 package scenes;
 
 import com.uqbar.vainilla.Game;
-import com.uqbar.vainilla.GameComponent;
-import com.uqbar.vainilla.GameScene;
-import com.uqbar.vainilla.UnitVector2D;
-import com.uqbar.vainilla.colissions.CollisionDetector;
-import components.Ball;
-import components.Platform;
-import components.ScoreBoard;
 import components.blocks.BlockFactory;
 import utils.YUpdater;
 
-import java.awt.*;
+public class ArkanoidScene extends BasicArkanoidScene {
 
-public class ArkanoidScene extends GameScene {
-	
-	private ScoreBoard scoreBoard = new ScoreBoard(10, 5, Color.black);
-	private Platform platform = new Platform(Color.blue, 10, 20, 580);
-	private Ball ball = new Ball(Color.black, 100, 390, new UnitVector2D(1, -1), 200);
 
-	public ArkanoidScene(Game game) {
-		super();
+    public ArkanoidScene(Game game) {
+        super(game);
+    }
 
-		this.setGame(game);
-		this.addComponent(this.getScoreBoard());
-		this.addBlocks();
-		this.addComponent(platform);
-		platform.center();
-		this.addComponent(ball);
-		ball.center();
-		ball.placeOver(platform);
-	}
-	
-	public void verifyBallCollides(Ball ball) {
-		for (GameComponent<?> component : this.getComponents()) {
-			if(component != ball && component != scoreBoard &&
-				CollisionDetector.INSTANCE.collidesCircleAgainstRect(ball.getCirc(), component.getRect())) {
-				
-				// Notify collides ball with component
-				ball.collide(component);
-				component.collide(ball);
-			}
-		}
-		if(this.getComponentCount() == 3) {
-			this.win();
-		}
-	}
-
-	protected void addBlocks() {
+    public void addBlocks() {
         BlockFactory.addBlocks(this, YUpdater.UPPER_LIMIT);
-	}
-	
-	public void lose() {
-		this.getGame().setCurrentScene(new LoseScene(this.getGame()));
-	}
-	
-	public void win() {
-		this.getGame().setCurrentScene(new WinScene(this.getGame()));
-	}
+    }
 
-	public void addScore(int value) {
-		this.getScoreBoard().add(value);
-	}
-	
-	public void speedUp(int value) {
-		this.getBall().speedUp(value);
-		// Increment platform speed??
-	}
+    public void win() {
+        this.getGame().setCurrentScene(new WinScene(this.getGame()));
+    }
 
-	public ScoreBoard getScoreBoard() {
-		return scoreBoard;
-	}
-	
-	public Ball getBall() {
-		return ball;
-	}
+
 }
---
+++ arkanoid/src/main/java/scenes/BasicArkanoidScene.java
@@ -0,0 +1,76 @@
+package scenes;
+
+import com.uqbar.vainilla.Game;
+import com.uqbar.vainilla.GameComponent;
+import com.uqbar.vainilla.GameScene;
+import com.uqbar.vainilla.UnitVector2D;
+import com.uqbar.vainilla.colissions.CollisionDetector;
+import components.Ball;
+import components.Platform;
+import components.ScoreBoard;
+
+import java.awt.*;
+
+public abstract class BasicArkanoidScene extends GameScene {
+    private ScoreBoard scoreBoard = new ScoreBoard(10, 5, Color.black);
+    private Platform platform = new Platform(Color.blue, 10, 20, 580);
+    private Ball ball = new Ball(Color.black, 100, 390, new UnitVector2D(1, -1), 200);
+
+    public BasicArkanoidScene(Game game) {
+        super();
+        this.setGame(game);
+        this.addComponent(this.getScoreBoard());
+        this.addBlocks();
+        this.addComponent(this.getPlatform());
+        this.getPlatform().center();
+        this.addComponent(this.getBall());
+        this.getBall().center();
+        this.getBall().placeOver(this.getPlatform());
+
+    }
+
+    public void verifyBallCollides(Ball ball) {
+        //TODO: refactor this.
+        for (GameComponent<?> component : this.getComponents()) {
+            if(component != ball && component != scoreBoard &&
+                    CollisionDetector.INSTANCE.collidesCircleAgainstRect(ball.getCirc(), component.getRect())) {
+
+                // Notify collides ball with component
+                ball.collide(component);
+                component.collide(ball);
+            }
+        }
+        if(this.getComponentCount() == 3) {
+            this.win();
+        }
+    }
+
+    public void lose() {
+        //TODO: player should lost a life
+        this.getGame().setCurrentScene(new LoseScene(this.getGame()));
+    }
+
+    public abstract void win();
+
+    public abstract void addBlocks();
+
+    public void addScore(int value) {
+        this.getScoreBoard().add(value);
+    }
+
+    public void speedUp(int value) {
+        this.getBall().speedUp(value);
+    }
+
+    public ScoreBoard getScoreBoard() {
+        return scoreBoard;
+    }
+
+    public Platform getPlatform() {
+        return platform;
+    }
+
+    public Ball getBall(){
+        return ball;
+    }
+}
